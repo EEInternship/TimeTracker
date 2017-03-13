9fc8312eaddd9825a6ee0a5241da604452215f0e
Aleš Pernat <ales.pernat@student.um.si>
2017-03-13 12:36:26 +0100

    Added another activity and renamed one
    Added another activity and renamed one

---
+++ TimeTracker/app/src/main/java/com/example/eeintership/timetracker/MonthlyOverviewActivity.java
@@ -0,0 +1,13 @@
+package com.example.eeintership.timetracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MonthlyOverviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_monthlyoverview);
+    }
+}

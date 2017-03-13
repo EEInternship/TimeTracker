ec0b29e1ac9d55db323734fbc72d80a2a6aa2450
Klemen Ledinek <klemen.ledinek@gmail.com>
2017-03-13 13:17:52 +0100

    Test2
    Test2

--- TimeTracker/app/src/main/java/com/example/eeintership/timetracker/MainActivity.java
+++ TimeTracker/app/src/main/java/com/example/eeintership/timetracker/MainActivity.java
@@ -4,10 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-//Klemen was here
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void test(){
+        System.out.println("Test");
+        //Klemen was also here
     }
 }

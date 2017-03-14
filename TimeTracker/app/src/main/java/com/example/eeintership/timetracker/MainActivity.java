5cbd0c457c12f0192f3511f81bae550b3f31090a
IsakFe <isak.fehric@gmail.com>
2017-03-14 15:54:27 +0100

    Restoring shadows and colours
    14.3.2017

--- TimeTracker/app/src/main/java/com/example/eeintership/timetracker/MainActivity.java
+++ TimeTracker/app/src/main/java/com/example/eeintership/timetracker/MainActivity.java
@@ -1,17 +1,21 @@
 package com.example.eeintership.timetracker;
 
 import android.content.res.Resources;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    private LinearLayout ly_open_close;
     private Button btn_open_close;
     int open = 0;
 
+    private LinearLayout ly_break_back;
     private Button btn_break_back;
     int breakBack = 0;
 
@@ -25,64 +29,114 @@ public class MainActivity extends AppCompatActivity {
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
 
+        ly_open_close = (LinearLayout) this.findViewById(R.id.background_shadow_open);
         btn_open_close = (Button) this.findViewById(R.id.button_open);
         btn_open_close.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (open == 0) {
-                    Drawable myD=null;
-                    Resources res=getResources();
-                    try{
-                        myD=Drawable.createFromXml(res,res.getXml(R.xml.btn_design_orange));
-                    }catch (Exception e){
+                    // for button change
+                    Drawable myD = null;
+                    Resources res = getResources();
+                    try {
+                        myD = Drawable.createFromXml(res, res.getXml(R.xml.btn_design_orange));
+                    } catch (Exception e) {
                         // if something goes wrong.
                     }
-
                     btn_open_close.setBackground(myD);
+
+                    // for shadow change
+                    Drawable myDs = null;
+                    Resources resS = getResources();
+                    try {
+                        myDs = Drawable.createFromXml(resS, resS.getXml(R.xml.btn_design_orange_shadow));
+                    } catch (Exception e) {
+                        // if something goes wrong.
+                    }
+                    ly_open_close.setBackground(myDs);
+
+                    btn_open_close.setTextColor(Color.parseColor("#C0FFFFFF"));
                     open = 1;
                     btn_open_close.setText("Close");
-                }else {
-                    Drawable myD=null;
-                    Resources res=getResources();
-                    try{
-                        myD=Drawable.createFromXml(res,res.getXml(R.xml.btn_design_green));
-                    }catch (Exception e){
+                } else {
+                    // for button change
+                    Drawable myD = null;
+                    Resources res = getResources();
+                    try {
+                        myD = Drawable.createFromXml(res, res.getXml(R.xml.btn_design_green));
+                    } catch (Exception e) {
                         // if something goes wrong.
                     }
-
                     btn_open_close.setBackground(myD);
+
+                    // for shadow change
+                    Drawable myDs = null;
+                    Resources resS = getResources();
+                    try {
+                        myDs = Drawable.createFromXml(resS, resS.getXml(R.xml.btn_design_green_shadow));
+                    } catch (Exception e) {
+                        // if something goes wrong.
+                    }
+                    ly_open_close.setBackground(myDs);
+
+                    btn_open_close.setTextColor(Color.parseColor("#FFFFFF"));
                     open = 0;
                     btn_open_close.setText("Open");
                 }
             }
         });
 
+        ly_break_back = (LinearLayout) this.findViewById(R.id.background_shadow_break);
         btn_break_back = (Button) this.findViewById(R.id.button_break);
         btn_break_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (breakBack == 0) {
-                    Drawable myD=null;
-                    Resources res=getResources();
-                    try{
-                        myD=Drawable.createFromXml(res,res.getXml(R.xml.btn_design_orange));
-                    }catch (Exception e){
+                    // for button change
+                    Drawable myD = null;
+                    Resources res = getResources();
+                    try {
+                        myD = Drawable.createFromXml(res, res.getXml(R.xml.btn_design_orange));
+                    } catch (Exception e) {
                         // if something goes wrong.
                     }
-
                     btn_break_back.setBackground(myD);
+
+                    // for shadow change
+                    Drawable myDs = null;
+                    Resources resS = getResources();
+                    try {
+                        myDs = Drawable.createFromXml(resS, resS.getXml(R.xml.btn_design_orange_shadow));
+                    } catch (Exception e) {
+                        // if something goes wrong.
+                    }
+                    ly_break_back.setBackground(myDs);
+
+                    btn_break_back.setTextColor(Color.parseColor("#C0FFFFFF"));
                     breakBack = 1;
                     btn_break_back.setText("Back");
                 } else {
-                    Drawable myD=null;
-                    Resources res=getResources();
-                    try{
-                        myD=Drawable.createFromXml(res,res.getXml(R.xml.btn_design_green));
-                    }catch (Exception e){
+                    // for button change
+                    Drawable myD = null;
+                    Resources res = getResources();
+                    try {
+                        myD = Drawable.createFromXml(res, res.getXml(R.xml.btn_design_green));
+                    } catch (Exception e) {
                         // if something goes wrong.
                     }
-
                     btn_break_back.setBackground(myD);
+
+                    // for shadow change
+                    Drawable myDs = null;
+                    Resources resS = getResources();
+                    try {
+                        myDs = Drawable.createFromXml(resS, resS.getXml(R.xml.btn_design_green_shadow));
+                    } catch (Exception e) {
+                        // if something goes wrong.
+                    }
+                    ly_break_back.setBackground(myDs);
+
+                    btn_break_back.setTextColor(Color.parseColor("#FFFFFF"));
                     breakBack = 0;
                     btn_break_back.setText("Break");
                 }

3067bf5c7be4dca92c7d99506398e5e11050174f
Aleš Pernat <ales.pernat@student.um.si>
2017-03-14 14:02:15 +0100

    MonthlyOverview
    Added activitiy opening on button click off Monthly Overview

--- TimeTracker/app/src/main/java/com/example/eeintership/timetracker/MonthlyOverviewActivity.java
+++ TimeTracker/app/src/main/java/com/example/eeintership/timetracker/MonthlyOverviewActivity.java
@@ -3,9 +3,10 @@ package com.example.eeintership.timetracker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MonthlyOverviewActivity extends AppCompatActivity {
-
+    private TextView textViewMonthlyOverview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +16,7 @@ public class MonthlyOverviewActivity extends AppCompatActivity {
         View decorView = getWindow().getDecorView();
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
+
+        textViewMonthlyOverview=(TextView)findViewById(R.id.tvMonthlyOverview);
     }
 }
